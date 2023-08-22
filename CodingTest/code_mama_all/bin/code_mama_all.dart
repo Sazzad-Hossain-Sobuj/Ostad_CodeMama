4f130664e1abf41f46015a1e0cefbd6140fc62df
Sazzad-Hossain-Sobuj <sazzad15-2596@diu.edu.bd>
2023-08-22 17:55:51 +0600

    initial commit

---
+++ CodingTest/code_mama_all/bin/code_mama_all.dart
@@ -0,0 +1,5 @@
+import 'package:code_mama_all/code_mama_all.dart' as code_mama_all;
+
+void main(List<String> arguments) {
+  print('Hello world: ${code_mama_all.calculate()}!');
+}
---
+++ CodingTest/code_mama_all/test/code_mama_all_test.dart
@@ -0,0 +1,8 @@
+import 'package:code_mama_all/code_mama_all.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
