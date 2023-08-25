606db5bf892ca04684e346eed1b786f37aa252ab
Sazzad Hossain <93114191+Sazzad-Hossain-Sobuj@users.noreply.github.com>
2023-08-25 11:43:34 +0600

    difference of two number

---
+++ CodingTest/code_mama_all/CodeMama/problem5_finding_difference_two_number.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main(){
+  String input = stdin.readLineSync()!;
+  List<String> num = input.split(' ');
+  int n1 = int.parse(num[0]);
+  int n2 = int.parse(num[1]);
+  int d = n1-n2;
+  print(d);
+
+}
