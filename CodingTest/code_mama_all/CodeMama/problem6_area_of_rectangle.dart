8375b94164975bdbbfdddf5a4cc4131e42f8e65c
Sazzad Hossain <93114191+Sazzad-Hossain-Sobuj@users.noreply.github.com>
2023-08-27 09:21:24 +0600

    area of rectangle

---
+++ CodingTest/code_mama_all/CodeMama/problem6_area_of_rectangle.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main(){
+  String input = stdin.readLineSync()!;
+  List<String> n = input.split(' ');
+
+  int length = int.parse(n[0]);
+  int width = int.parse(n[1]);
+  int area = length*width;
+  print(area);
+}
