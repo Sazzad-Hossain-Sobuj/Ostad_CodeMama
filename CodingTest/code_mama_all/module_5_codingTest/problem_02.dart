66e66aacf3ad11cb95f7bab42c9cf3d7e24660b4
Sazzad-Hossain-Sobuj <sazzad15-2596@diu.edu.bd>
2023-09-03 20:54:28 +0600

    problem 02

--- CodingTest/code_mama_all/module_5_codingTest/problem_02.dart
+++ CodingTest/code_mama_all/module_5_codingTest/problem_02.dart
@@ -1,41 +1,14 @@
 import 'dart:io';
 
 void main() {
-  String? S = stdin.readLineSync();
+  String S = stdin.readLineSync()!;
+  int T = int.parse(stdin.readLineSync()!);
 
-  if (S == null || S.length != 5) {
-    print("Invalid input.");
-    return;
+  if (S == "casual" && T >= 15 && T <= 25) {
+    print("Jeans and a light jacket.");
+  } else if (S == "festive" && T > 25) {
+    print("Colorful dress and sandals.");
+  } else {
+    print("Wear what you're comfortable in.");
   }
-  String firstOperand = S[0];
-  String operator = S[2];
-  String secondOperand = S[4];
-
-  int num1 = int.tryParse(firstOperand) ?? 0;
-  int num2 = int.tryParse(secondOperand) ?? 0;
-
-  int result;
-  switch (operator) {
-    case '+':
-      result = num1 + num2;
-      break;
-    case '-':
-      result = num1 - num2;
-      break;
-    case '*':
-      result = num1 * num2;
-      break;
-    case '/':
-      if (num2 == 0) {
-        print("Division by zero is not possible.");
-        return;
-      }
-      result = num1 ~/ num2;
-      break;
-    default:
-      print("Invalid operator.");
-      return;
-  }
-
-  print("$result");
 }
