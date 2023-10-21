e78597f14be30216ca46bc454759862c5a5b32c5
Sazzad-Hossain-Sobuj <sazzad15-2596@diu.edu.bd>
2023-10-21 18:07:58 +0600

    decition making or conditional statement

---
+++ Dart practice/Dart all/dart_all_online_practice/lib/decision_making_conditionall_statement.dart
@@ -0,0 +1,25 @@
+void main() {
+  String connection = 'connected';
+
+  // if(connection == 'connected'){
+  //   print('connected');
+  // }
+  // else if (connection == 'waiting'){
+  //   print('waiting');
+  // }
+  // else {
+  //   print('disconnected');
+  // }
+  switch (connection) {
+    case 'connected':
+      print('connected');
+      break;
+
+    case 'waiting':
+      print('waiting');
+      break;
+
+    default:
+      print('disconnected');
+  }
+}
