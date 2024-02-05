9bc96677c3e670cfa93e4de9b84eeae96c8a2aab
Sazzad Hossain <93114191+sazzad-2596@users.noreply.github.com>
2024-02-05 22:10:55 +0600

    problem_1

--- CodingTest/code_mama_all/CodeMama/code_mama_16_to_18/module_21/problem_1.dart
+++ CodingTest/code_mama_all/CodeMama/code_mama_16_to_18/module_21/problem_1.dart
@@ -1,19 +1,21 @@
 import 'dart:io';
 
 void main() {
-  String input = stdin.readLineSync()!;
-  int number = int.tryParse(input) ?? 0;
-  bool isRepdigit = checkRepdigit(number);
-  print(isRepdigit);
-}
+  String str = stdin.readLineSync()!;
+  str = str.trim();
+  var map = {
+    'zero': 0,
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9,
+  };
 
-bool checkRepdigit(int num) {
-  String numStr = num.toString();
-  for (int i = 1; i < numStr.length; i++) {
-    if (numStr[i] != numStr[0]) {
-      return false;
-    }
-  }
-
-  return true;
+  int k = map[str]!;
+  print( k % 2 );
 }
